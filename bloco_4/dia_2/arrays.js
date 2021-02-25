84767cba18954a9fc8ed2eb62e1894ac52504646
Debora Passos <deborinhapassos@gmail.com>
2021-02-25 15:15:33 -0300

    Exercicios Array

---
+++ bloco_4/dia_2/arrays.js
@@ -0,0 +1,17 @@
+//exercício 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("Serviços");
+
+console.log(menuServices);
+
+//exercício 2
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//exercício 3
+
+menu.push = ('Contato');
+
+console.log(menu);
