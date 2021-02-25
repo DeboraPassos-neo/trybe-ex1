68b4ad398a137cf8ffb6962d76da910073f4a572
Debora Passos <deborinhapassos@gmail.com>
2021-02-25 14:32:57 -0300

    Exercicios de 1 a 5

---
+++ bloco_4/dia_1/script.js
@@ -0,0 +1,74 @@
+/*questão1 */
+let a = 10;
+let b = 20;
+
+let ab = a + b;
+  console.log(ab);
+
+let abSub = a - b;
+  console.log(abSub);
+
+let abMult = a * b;
+  console.log(abMult);
+
+let abDiv = a / b;
+  console.log(abDiv);
+
+let abMod = a % b;
+  console.log(abMod);
+
+  /*questão 2 */
+let maior = 40;
+let menor = 30;
+
+let compar = maior < menor; 
+  console.log(compar);
+
+/*questão 3 */
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+let numM = num1;
+  
+if (num2 > num1 && num2 > num3) {
+  console.log(numM = num2);
+}
+
+else if (num3 > num2 && num3 > num1) {
+  console.log(numM = num3);
+}
+
+else {
+  console.log("numero inexistente")
+}
+
+/*questão 4 */
+let ex4 = -4;
+
+if (ex4 >= 1) {
+  console.log("positive");
+}
+else if (ex4 <= -1 ) {
+  console.log("negative");
+}
+
+else {
+  console.log("zero");
+}
+
+/*questão 5 */
+let ang1 = 90;
+let ang2 = 80;
+let ang3 = 120;
+let soma = ang1 + ang2 + ang3;
+let res;
+
+if (soma == 180) {
+  res = true;
+} else {
+  res = false;
+}
+console.log(res)
+
+/* questão 6 */
