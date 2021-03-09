480d83ac533c968431cabd35a70af1807b981154
Debora Passos <deborinhapassos@gmail.com>
2021-03-09 10:48:35 -0300

    exercicios 5.1

--- bloco_4/dia_2/exercicio_1.js
+++ bloco_4/dia_2/exercicio_1.js
@@ -6,6 +6,7 @@ for(let index = 0; index < numbers.length; index += 1){
 }
 
 //exercicio 2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 
 for (let sumIndex = 0; sumIndex < numbers.length; sumIndex += 1){
@@ -14,12 +15,13 @@ for (let sumIndex = 0; sumIndex < numbers.length; sumIndex += 1){
 console.log(sum);
 
 //exercicio 3
-
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let average = sum / numbers.length
 
 console.log(average);
 
 //exercício 4
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 if (average > 20){
   console.log('valor maior que 20');
 }else{
@@ -27,13 +29,51 @@ if (average > 20){
 }
 
 //exercicio 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let larger = 0; 
 
-for (indexLarger< 0;indexLarger < numbers.length; indexLarger += 1) {
-  if(numbers[indexLarger] > larger);{
-  console.log(larger);
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if(numbers[index] > larger) {
+    larger = numbers[index];
   }
 }
+  console.log(larger);
+//exercício 6
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let odd = 0;
 
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    odd += 1;
+  }
+}
+  if (odd === 0) {
+  console.log('nenhum valor ímpar encontrado');
+  } else {
+  console.log(odd);
+  }
 
+//exercício 7
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let lessNumber = 0;
 
+for (let index = 0; index < numbers.length; index += 1) {
+  if(numbers[index] < lessNumber) {
+    lessNumber = numbers[index];
+  }
+}
+  console.log(lessNumber);
+
+// exercício 8
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+   numbers.push(index);
+}
+console.log(numbers);
+
+//exercicio 9
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i] / 2);
+};
